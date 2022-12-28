75504485ee6b38ed89297f770363f6e085fdd3d3
vseguin <vincent.seguin@carta.com>
2022-12-28 15:28:31 -0500

    feat(): add brand creation

--- pages/api/admin/brands.ts
+++ pages/api/admin/brands.ts
@@ -1,16 +1,14 @@
-import { wanted_cars } from "@prisma/client";
+import { brands } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../lib/prisma";
-import { v4 as uuidv4 } from "uuid";
 import Joi from "joi";
 import validate from "../../../lib/middlewares/validation";
 
-type Response = wanted_cars;
+type Response = brands;
 
 const schema = Joi.object({
-  brand: Joi.string().required(),
-  model: Joi.string().required(),
-  maker: Joi.string().required(),
+  name: Joi.string().required(),
+  country: Joi.string().required(),
 });
 
 const handler = async function (
@@ -18,15 +16,13 @@ const handler = async function (
   res: NextApiResponse<Response>
 ) {
   if (req.method === "POST") {
-    const wanted = await prisma.wanted_cars.create({
+    const brand = await prisma.brands.create({
       data: {
-        id: uuidv4(),
-        brand: req.body.brand,
-        model: req.body.model,
-        maker: req.body.maker,
+        name: req.body.name,
+        country: req.body.country,
       },
     });
-    res.status(200).json(wanted);
+    res.status(200).json(brand);
   }
 };
 
