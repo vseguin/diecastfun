026d8f4538cb39ed7dab77e142b28345e6c25a48
vseguin <vincent.seguin@carta.com>
2022-12-28 07:54:07 -0500

    feat(): add custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <>
+      <h1>What are you doing here?</h1>
+      <Link href="/">Go back home</Link>
+    </>
+  );
+}
